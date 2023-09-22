e1cb82b43251be1818309defa6dc1ca7f407a10e
nicoleneza <rwegoneza12@gmail.com>
2023-09-22 15:36:40 +0200

    another commit

--- app/components/shared/Footer.jsx
+++ app/components/shared/Footer.jsx
@@ -51,66 +51,6 @@ const Footer = () => {
                         Contact Us
                     </Link>
                 </div>
-         {/* <div  className=''>
-            <span className='text-4xl'>BudgeLine</span>
-            </div>
-        <div className='flex '>
-            
-        <div className='flex flex-col'>
-            <span className='font-bold text-2xl'>Explore</span>
-            <div className='flex flex-col text-lg'>
-            <Link href='/Home' className='' >
-                Home
-            </Link>
-            <Link href='/Services' className=''  >
-                Services
-            </Link>
-            <Link href='/About' className=''>
-                How it works
-            </Link>
-            <Link href='/Reviews' className=''>
-                Reviews
-            </Link>
-            </div>
-        </div>
-
-        <div className=' flex flex-col text-2xl'>
-            <div>
-                <span className='font-bold '>Get started</span>
-                </div>
-
-                <div className='flex flex-col text-lg '>
-                    <Link 
-                       href='/Login' 
-                       className='' >
-                        Sign Up
-                    </Link>
-                    <Link 
-                        href='/Register'
-                        className='' >
-                        Log In
-                    </Link>
-                    <Link 
-                        href='' 
-                        className='Online budget goals'>
-                    </Link>
-                    <Link 
-                        href='' 
-                        className='How to make a budget'>
-                    </Link>
-                    </div>
-        </div>
-        <div className='flex flex-col text-2xl'>
-            <div>
-                <span className='font-bold'>Get help</span>
-            </div>
-            <div className='flex flex-col text-lg'>
-                <Link href=''>How to videos</Link>
-                <Link href=''>Contact Us</Link>
-            </div>
-        </div>
-        </div> */}
-
 
         </footer>
     // </div>
